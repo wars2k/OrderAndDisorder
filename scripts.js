d9372c4a4cb572e1ba7fea636bbc1f184458d48d
wars2k <wars2kpineapple@gmail.com>
2022-11-26 12:53:42 -0500

    lots of JS progress

--- scripts.js
+++ scripts.js
@@ -1,28 +1,43 @@
 
 function getVerse(book,chapter,verse) {
+	//remove any already present permanent highlights
 	var highlightClass = document.getElementsByClassName("permanentHighlight");
 	while (highlightClass.length)
 		highlightClass[0].classList.remove("permanentHighlight");
+	//add a permanent highlight to the line just clicked
 	highlight = event.target
 	highlight.classList.add("permanentHighlight");
-	console.log(event.target);
+	//get the requested translation
+	var select = document.getElementById("translationSelection");
+	var value = select.value;
+	//fetch the data
 	test = verse;
-    fetch('https://bible-api.com/' + book + '+' + chapter + '?translation=kjv')
+    fetch('https://bible-api.com/' + book + '+' + chapter + '?translation=' + value)
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
-    displayChapter(data);
+    displayBible(data);
 
   })
     .catch(function() {
     // catch any errors
   });
 }
-
+function displayBible(data) {
+	var select = document.getElementById("referenceSelection");
+	var value = select.value;
+	if(value == "chapter") {
+		displayChapter(data);
+	} else if (value == "verse") {
+		displayVerse(data);
+	} else {
+		displayVerseInContext(data);
+	}
+}
 function displayChapter(data) {
-	bibleBox = document.getElementById("bibleBox");
+	bibleBox = document.getElementById("referencePageContent");
 	bibleBox.innerHTML = "";
-	title = document.getElementById("titleRight");
-	title.innerHTML = "<h1>" + data.reference + ":" + test + "</h1>";
+	//title = document.getElementById("titleRight");
+	//title.innerHTML = "<h1>" + data.reference + ":" + test + "</h1>";
 
 	for (i = 0; i < data.verses.length; i++) {
 		number = i+1;
@@ -35,3 +50,43 @@ function displayChapter(data) {
 	}
 }
 
+function displayVerse(data) {
+	bibleBox = document.getElementById("referencePageContent");
+	bibleBox.innerHTML = "";
+	test = test - 1;
+	verse = document.createElement("div");
+	bibleBox.append(verse);
+	verse.innerHTML = data.verses[test].text + " <b>(" + data.reference + ":" + test + ")</b>";
+
+}
+
+
+
+function LineNumbers() {
+	var wrapper = document.getElementsByClassName("lineWrapper")
+	for (i=0; i < wrapper.length; i++) {
+		counter = document.createElement("div");
+		counter.classList.add("lineNumber");
+		counter.innerText = i+1;
+		wrapper[i].prepend(counter);
+	}
+}
+//this function only needs to run once when the page opens. 
+LineNumbers()
+
+function changeLineNumbers() {
+	var select = document.getElementById("lineNumbers");
+	var value = select.value;
+	console.log(value);
+	if (value == "true") {
+		const lineNumbers = document.querySelectorAll('.lineNumber');
+		lineNumbers.forEach(lineNumber => {
+			lineNumber.style.display = "inline";
+		});
+	} else {
+		const lineNumbers = document.querySelectorAll('.lineNumber');
+		lineNumbers.forEach(lineNumber => {
+			lineNumber.style.display = "none";
+		});
+	}
+}
